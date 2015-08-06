6476cdbee7acf52139fd60ec21733c26e17e16af
ChipMorris10 <goldcomac@mac.com>
2015-08-06 16:02:49 -0600

    Finished comments

--- main.js
+++ main.js
@@ -1,61 +1,100 @@
-// prompt how many students learning JS
-// need to turn numStudents into an integer
+// Purpose: to match up students with mentors
+// Requirements:
+  // Prompt how many students taking JS Class
+  // Prompt for student Name, Phone, City
+  // Alert answers, formatted for readability
+  // Repeat the same for mentors
+
+// Refactor:
+  // Instead of prompting for number of students, start with student info
+    // and prompt to see if more information will be entered
+  // Repeat for mentors
+
+// Bonus:
+  // Print results to the DOM instead of alerting the answers
+  // Create a form instead of alerting for information - I didn't do this
+  // I kept the alerts *and* printed to the DOM
+
+
+
+// global variables used throughout
+var students = promptForInformation("student");
+var mentors = promptForInformation("mentor");
+
+// create function to capitalize the first letter in the name
 function capitalizeFirstLetter(string) {
 return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+// function to run Name, Phone, and City prompts
 function runPrompts(type) {
-var studentName = prompt("What is the " + type + "'s name? ");
-var studentPhoneNo = checkPhoneNumber(type);
-var studentCity = prompt("What city does the " + type + " live in?");
-return {name: studentName, number: studentPhoneNo, city:studentCity};
+
+  var studentName = prompt("What is the " + type + "'s name? ");
+
+  var studentPhoneNo = checkPhoneNumber(type);
+
+  var studentCity = prompt("What city does the " + type + " live in?");
+    return {name: studentName, number: studentPhoneNo, city:studentCity};
 }
-// create empty array for totalStudents
-// collect studentName, studentPhone, studentCity
-function gatherData(type) {
-var results = [];
-var keepGoing = true;
-while (keepGoing) {
-var result = runPrompts(type);
-results.push(result);
-keepGoing = confirm("Would you like to continue?");
-}
-return results;
-}
+
+// function to validate phone number convention
 function validatePhoneNumber(studentPhoneNo, type) {
-while (studentPhoneNo.length != 12 || studentPhoneNo.charAt(3) != "-" || studentPhoneNo.charAt(7) != "-") {
-alert(" Your phone number is not valid, please re-enter your number using the format xxx-xxx-xxxx ");
-studentPhoneNo = prompt("What is the " + type + "'s phone number? (xxx-xxx-xxxx)");
+  while (studentPhoneNo.length != 12 || studentPhoneNo.charAt(3) != "-" || studentPhoneNo.charAt(7) != "-") {
+    alert(" Your phone number is not valid, please re-enter your number using the format xxx-xxx-xxxx ");
+    studentPhoneNo = prompt("What is the " + type + "'s phone number? (xxx-xxx-xxxx)");
+  }
+    return studentPhoneNo;
 }
-return studentPhoneNo;
-}
+
+// function to gather phone number
 function checkPhoneNumber(type) {
-var studentPhoneNo = prompt("What is the " + type + "'s phone number? (xxx-xxx-xxxx)");
-studentPhoneNo = validatePhoneNumber(studentPhoneNo, type);
-return studentPhoneNo;
-}
+  var studentPhoneNo = prompt("What is the " + type + "'s phone number? (xxx-xxx-xxxx)");
+  studentPhoneNo = validatePhoneNumber(studentPhoneNo, type);
+  return studentPhoneNo;
+  }
+
+// function to display information in an alert
 function displayData(results) {
-for (var i = 0; i < results.length; i ++) {
-alert(" Name: " + capitalizeFirstLetter(results[i].name) + " \n Phone: " + results[i].number + "\n City: " + results[i].city.toUpperCase());
+  for (var i = 0; i < results.length; i ++) {
+  alert(" Name: " + capitalizeFirstLetter(results[i].name) + " \n Phone: " + results[i].number + "\n City: " + results[i].city.toUpperCase());
+  }
 }
-}
+
+// function to gather data
 function promptForInformation(type) {
-var results = gatherData(type);
-displayData(results);
+  var results = gatherData(type);
+  displayData(results);
+  return results;
+}
+
+// function to collect information to insert on the DOM
+function insertsDOM (object) {
+  var list = document.getElementById('data');
+  var li = document.createElement('li');
+  var name = document.createTextNode("NAME: " + object.name + ", PHONE: " + object.number + ", CITY: " + object.city);
+  li.appendChild(name);
+  list.appendChild(li);
+}
+
+// function to display information on the DOM
+function displayDOM (array) {
+  array.forEach(function(name) {
+  insertsDOM(name);
+  });
+}
+
+// function to determine if more info will be collected
+function gatherData(type) {
+  var results = [];
+  var keepGoing = true;         // returns true by default
+    while (keepGoing) {
+    var result = runPrompts(type);
+    results.push(result);
+    keepGoing = confirm("Would you like to continue?");
+  }
 return results;
 }
-var students = promptForInformation("student");
-var mentors = promptForInformation("mentor");
-// console.log(totalStudents);
-function insertsDOM (object) {
-var list = document.getElementById('data');
-var li = document.createElement('li');
-var name = document.createTextNode("name: " + object.name + ", phone: " + object.phone);
-li.appendChild(name);
-list.appendChild(li);
-}
-function displayDOM (array) {
-array.forEach(function(name) {
-insertsDOM(name);
-});
-}
+
+// display results on the DOM
 displayDOM(students);
+displayDOM(mentors);
